38d8743baf04dea847b3fc9a3741b3502289511c
YAK <yakxuanhong11300@gmail.com>
2022-05-09 23:14:58 +0800

    user.test pending

--- user.js
+++ user.js
@@ -1,26 +1,115 @@
-let users;
+const bcrypt = require("bcrypt");
+const { mongoConnection } = require("./connection");
 
-class User {
-	static async injectDB(conn) {
-		users = await conn.db("my-database-name").collection("users")
-	}
 
-	static async register(username, password) {
-		// TODO: Check if username exists
+  users = await conn.db("my-database-name").collection("users")
 
-		// TODO: Hash password
+/**
+ * @addUser
+ */
 
-		// TODO: Save user to database
-	}
+function register(userData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // check if user does not exists
+      let checkUserData = await checkIfUserExists({ email: userData.email });
+      if (checkUserData.data && checkUserData.data.length > 0) {
+        // user already exists, send response
+        return resolve({
+          error: true,
+          message: "User already exists with this credentials. Please login",
+          data: [],
+        });
+      }
+      // generate password hash
+      let passwordHash = await bcrypt.hash(userData.password, 15);
+      userData.password = passwordHash;
 
-	static async login(username, password) {
-		// TODO: Check if username exists
-
-		// TODO: Validate password
-
-		// TODO: Return user object
-		return;
-	}
+      // add new user
+      mongoConnection
+        .collection("users")
+        .insertOne(userData, async (err, results) => {
+          if (err) {
+            console.log(err);
+            throw new Error(err);
+          }
+          //return data
+          resolve({
+            error: false,
+            data: results.ops[0],
+          });
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
-module.exports = User;
+/**
+ * @verifyUser
+ * @param {*} userData
+ */
+
+function login(userData) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let userDatafromDb = await checkIfUserExists({ email: userData.email });
+      if (userDatafromDb.data && userDatafromDb.data.length > 0) {
+        // user already exists, verify the password
+        let passwordVerification = await bcrypt.compare(
+          userData.password,
+          userDatafromDb.data[0].password
+        );
+        if (!passwordVerification) {
+          // password mismatch
+          return resolve({
+            error: true,
+            message: "Invalid email or password",
+            data: [],
+          });
+        }
+        // password verified
+        return resolve({ error: false, data: userDatafromDb.data[0] });
+      } else {
+        return resolve({
+          error: true,
+          message:
+            "There is no user exists with this credentials. Please create a new account.",
+          data: [],
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      reject(e);
+    }
+  });
+}
+
+/**
+ * @checkIfUserExists
+ */
+
+function checkIfUserExists(userData) {
+  return new Promise((resolve, reject) => {
+    try {
+      // check if user exists
+      mongoConnection
+        .collection("users")
+        .find({ email: userData.email })
+        .toArray((err, results) => {
+          if (err) {
+            console.log(err);
+            throw new Error(err);
+          }
+          resolve({ error: false, data: results });
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+module.exports = {
+  register: register,
+  login: login,
+};
--- user.test.js
+++ user.test.js
@@ -1,6 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
 const User = require("./user")
 
+let userData = {
+	email: "alice@gmail.com",
+  	password: "abc123",
+}
+
 describe("User Account", () => {
 	let client;
 	beforeAll(async () => {
@@ -16,27 +21,27 @@ describe("User Account", () => {
 	})
 
 	test("New user registration", async () => {
-		const res = await User.register("test", "test")
-		expect().toBe()
+		const res = await User.register(userData)
+		expect(res.data).toBe(userData)
 	})
 
 	test("Duplicate username", async () => {
-		const res = await User.register("test", "test")
-		expect().toBe()
+		const res = await User.register(userData)
+		expect(res.error).toBe(true)
 	})
 
-	test("User login invalid username", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
-	})
+	// test("User login invalid username", async () => {
+	// 	const res = await User.login("test", "test")
+	// 	expect().toBe()
+	// })
 
-	test("User login invalid password", async () => {
-		const res = await User.login("test", "test")
-		expect().toBe()
-	})
+	// test("User login invalid password", async () => {
+	// 	const res = await User.login("test", "test")
+	// 	expect().toBe()
+	// })
 
-	test("User login successfully", async () => {
-		const res = await User.login("test", "test")
-		expect(res).toBe(true)
-	})
+	// test("User login successfully", async () => {
+	// 	const res = await User.login("test", "test")
+	// 	expect(res).toBe(true)
+	// })
 });
